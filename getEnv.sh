b63b624ebfc0ceee79818f72528acf258e3acf56
Fei <fei.yan@nokia.com>
2016-05-09 17:37:24 +0800

    syntax check for getEnv.sh

--- getEnv.sh
+++ getEnv.sh
@@ -3,38 +3,40 @@ source ~/bin/funcs.sh
 bldtop=/build/ltesdkroot/
 excludeFolder=~/bin/rsync-excludes/
 
+function syncForPart(){
+    comp=$1
+    fullDir=$2
+    if [ ! -z "$comp" ];then
+        echo "Updating $PS_REL using rsync"
+        syncFolder "$fullDir"
+        [ $? != 0 ] && exit 1
+    else
+        echo "Invalid component=$comp"
+        exit 101
+    fi
+}
+
 ## 1> ECL/ECL >> PS_REL_ -> $bldtop/Platforms/PS_REL/$PS_REL
-PS_REL=`cat ECL/ECL| sed -n "s/^ECL_PS_REL=//gp"`
-echo "Updating $PS_REL using rsync"
-#syncFolder $bldtop/Platforms/PS_REL/$PS_REL ${excludeFolder}/ps.txt
-syncFolder $bldtop/Platforms/PS_REL/$PS_REL
-[ $? != 0 ] && exit 1
+PS_REL=$(sed -n "s/^ECL_PS_REL=//gp" ECL/ECL)
+syncForPart "$PS_REL" "$bldtop/Platforms/PS_REL/$PS_REL"
 
 ## Step 2> PS_REL >> PS_LFS_REL -> $bldtop/Platforms/LINUX/$PS_LFS_REL
-PS_LFS_REL=`cat $bldtop/Platforms/PS_REL/$PS_REL/BTS_PS_versionfile.txt | sed -n  "s/^LFS_REL=//gp"`
-echo "Updating $PS_LFS_REL using rsync"
-#syncFolder $bldtop/Platforms/LINUX/$PS_LFS_REL ${excludeFolder}/ps_lfs.txt
-syncFolder $bldtop/Platforms/LINUX/$PS_LFS_REL
-[ $? != 0 ] && exit 1
+PS_LFS_REL=$(sed -n "s/^LFS_REL=//gp" "$bldtop/Platforms/PS_REL/$PS_REL/BTS_PS_versionfile.txt")
+syncForPart "$PS_LFS_REL" "$bldtop/Platforms/LINUX/$PS_LFS_REL"
 
 ## Step 3> PS_LFS_REL >> SDK -> $bldtop/SDK/$PS_LFS_SDK
-PS_LFS_SDK=`readlink $bldtop/Platforms/LINUX/$PS_LFS_REL/sdk | awk -F "/" '{print $NF}'`
-echo "Updating $PS_LFS_SDK using rsync"
-#syncFolder $bldtop/SDK/$PS_LFS_SDK ${excludeFolder}/sdk.txt
-syncFolder $bldtop/Platforms/SDK/$PS_LFS_SDK 
-[ $? != 0 ] && exit 1
+PS_LFS_SDK=$(readlink "$bldtop/Platforms/LINUX/$PS_LFS_REL/sdk" | awk -F "/" '{print $NF}')
+syncForPart "$PS_LFS_SDK" "$bldtop/Platforms/SDK/$PS_LFS_SDK"
 
 ## Step 4> $bldtop/Platforms/SDK/$PS_LFS_SDK (just a link)
 
 ## Step 5> PS_REL >> OSE_CK -> $bldtop/Platforms/OSE/$OSE_CK
-OSE_CK=`cat $bldtop/Platforms/PS_REL/$PS_REL/BTS_PS_versionfile.txt | sed -n  "s/^OSE_CK=//gp"`
-echo "Updating $OSE_CK using rsync"
-syncFolder $bldtop/Platforms/OSE/$OSE_CK
+OSE_CK=$(sed -n "s/^OSE_CK=//gp" "$bldtop/Platforms/PS_REL/$PS_REL/BTS_PS_versionfile.txt")
+syncForPart "$OSE_CK" "$bldtop/Platforms/OSE/$OSE_CK"
 
 ## Step 6> TOOLSET >> $bldtop/Tools/TOOLSET/$TOOLSET
-TOOLSET=`cat ECL/ECL| sed -n "s/^ECL_TOOLSET=//gp"`
-echo "Updating $TOOLSET using rsync"
-syncFolder $bldtop/Tools/TOOLSET/$TOOLSET
+TOOLSET=$(sed -n "s/^ECL_TOOLSET=//gp" ECL/ECL)
+syncForPart "$TOOLSET" "$bldtop/Tools/TOOLSET/$TOOLSET"
 
 ## Step 7> ISAR -- How to generate ISAR_SRC tag id?
 
@@ -42,8 +44,9 @@ syncFolder $bldtop/Tools/TOOLSET/$TOOLSET
 #       APPLENV -> $bldtop/Sacks/COMMON_APPL_ENV/$APPLENV
 #       GLOBALENV -> $bldtop/Sacks/GLOBAL_ENV/$GLOBALENV
 
-APPLENV=`cat ECL/ECL | sed -n "s/^ECL_COMMON_APPL_ENV=//gp"`
-syncFolder $bldtop/Sacks/COMMON_APPL_ENV/$APPLENV
-GLOBALENV=`cat ECL/ECL | sed -n "s/^ECL_GLOBAL_ENV=//gp"`
-syncFolder $bldtop/Sacks/GLOBAL_ENV/$GLOBALENV
+APPLENV=$(sed -n "s/^ECL_COMMON_APPL_ENV=//gp" ECL/ECL)
+syncForPart "$APPLENV" "$bldtop/Sacks/COMMON_APPL_ENV/$APPLENV"
+
+GLOBALENV=$(sed -n "s/^ECL_GLOBAL_ENV=//gp" ECL/ECL)
+syncForPart "$GLOBALENV" "$bldtop/Sacks/GLOBAL_ENV/$GLOBALENV"
 
